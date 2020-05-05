65e895648a4c8ad78d8f985dfaa396a2fc96b71d
shiotomo <t.shiota16@gmail.com>
2020-05-06 01:04:26 +0900

    slack botを起動できるようにした

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/shiotomo/health-check-slack
 
 go 1.14
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.6.4 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
+
+	if err != nil {
+		// can not read .env
+		fmt.Println("can not read .env")
+		panic(err)
+	}
+
+	token := os.Getenv("SLACK_BOT_TOKEN")
+
+	api := slack.New(
+		token,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		fmt.Print("Event Received: ")
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			rtm.SendMessage(rtm.NewOutgoingMessage("hello world", ev.Channel))
+		}
+	}
 }
